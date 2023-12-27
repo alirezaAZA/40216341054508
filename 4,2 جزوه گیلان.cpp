48c37f74cf903bd2aea77ec9c9e7929df7d9bf27
alirezaAZA <151343386+alirezaAZA@users.noreply.github.com>
2023-12-27 20:24:30 +0330

    Add files via upload

---
+++ 4,2 جزوه گیلان.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int a;
+    int a1,a2;
+    double result;
+    char operand;
+    char ch;
+    cout<<"enter the numbers:";
+    cin>>a1>>a2;
+    cout<<"enter operand:";
+    operand=getch();
+    putch(operand);
+    cout<<"\n";
+
+    switch (operand)
+    {
+        case'+':
+            result=a1+a2;
+            cout<<"a1+a2="<<result;
+            break;
+        case'-':
+            result=a1-a2;
+            cout<<"a1-a2="<<result;
+            break;
+        case'*':
+            result=a1*a2;
+            cout<<"a1*a2="<<result;
+        case'/':
+            if (a2==0)
+            {
+                cout<<"it is not possible";
+                break;
+            }
+            else
+            {
+                result=a1/a2;
+                cout<<"a1/a2="<<result;
+                break;
+            }
+        default:
+            cout<<"invalid operator!!!!!!!!";
+    }
+    getch();
+}
+
+
+
