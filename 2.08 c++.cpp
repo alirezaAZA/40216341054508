f294af8839a0cc69b7938ede902ad14a039d382f
alirezaAZA <151343386+alirezaAZA@users.noreply.github.com>
2023-12-27 21:04:31 +0330

    Add files via upload

---
+++ 2.08 c++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long int s=0,a;
+    cout<<"enter your numbers:"<<endl;
+    cin>>a;
+    while(a!=0)
+    {
+        s+=0;
+        cin>>a;
+    }
+    cout<<"sum is"<<s;
+}
+
+
+
